9f9ffb87d84fbb227d456aafe2186e7e3cb65aab
vadim010975 <vadim010975@mail.ru>
2024-02-06 20:32:53 +0300

    init

---
+++ src/ShopItemClass.jsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ShopItemClass extends Component {
+  render() {
+
+    const item = this.props.item;
+
+    return (
+      <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+    )
+  }
+}
+
+ShopItemClass.propTypes = {
+  item: PropTypes.shape({
+    brand: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number,
+    currency: PropTypes.string,
+  }),
+}
